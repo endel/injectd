26a61ae6edff35146fd4cac148682bfef4da7b3a
Endel Dreyer <endel.dreyer@gmail.com>
2016-06-26 17:49:17 +0200

    run cross-browser tests using karma on CI

---
+++ karma.conf.remote.js
@@ -0,0 +1,44 @@
+module.exports = function (config) {
+  return {
+    basePath: "",
+    frameworks: ["mocha", "chai"],
+    files: [
+      "./test/**.test.ts"
+    ],
+    preprocessors: {
+      "./test/**.test.ts": ["webpack"]
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true
+    },
+    plugins: [
+      require("karma-webpack"),
+      require("karma-mocha"),
+      require("karma-chai"),
+      require("karma-sauce-launcher"),
+    ],
+    reporters: ["dots", "saucelabs"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+
+    sauceLabs: {
+      testName: "injectd",
+      recordScreenshots: false,
+      connectOptions: {
+        port: 5757,
+        logfile: "sauce_connect.log"
+      },
+      public: "public"
+    },
+
+    // Increase timeout in case connection in CI is slow
+    captureTimeout: 120000,
+
+    customLaunchers: browsers,
+    browsers: Object.keys(browsers),
+    singleRun: true
+  }
+}
